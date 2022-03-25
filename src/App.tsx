62ad50f70c44c8052b907068d853b82d548afd31
mg <757057999@qq.com>
2022-03-25 10:55:50 +0800

    feat: button group

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,6 @@ import useSocket from "./useSocket";
 
 const App = () => {
   const [data, setData] = useSocket();
-
-  const onConnect = () => {};
-  const onDisconnect = () => {};
   return (
     <div className=" w-screen h-100 pb-8 bg-gradient-to-r from-indigo-50 to-indigo-100">
       <Helmet>
@@ -29,18 +26,7 @@ const App = () => {
 
       <AppRouter data={data.os}/>
       <Outlet />
-      {/* <button
-        onClick={onConnect}
-        className="box-border flex w-10/12 justify-center  font-serif py-4 mb-8 m-auto rounded-sm bg-green-500 text-black-300 text-sm"
-      >
-        链接
-      </button>
-      <button
-        onClick={onDisconnect}
-        className="box-border flex w-10/12 justify-center  font-serif py-4 m-auto rounded-sm bg-cyan-500 text-black-300 text-sm"
-      >
-        断开链接
-      </button> */}
+      
     </div>
   );
 };
---
+++ src/components/ButtonGroup.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const ButtonGroup = () => {
+
+  const onConnect = () => {};
+  const onDisconnect = () => {};
+	return <>
+	 <button
+        onClick={onConnect}
+        className="box-border flex w-10/12 justify-center  font-serif py-4 mb-8 m-auto rounded-sm bg-green-500 text-black-300 text-sm"
+      >
+        链接
+      </button>
+      <button
+        onClick={onDisconnect}
+        className="box-border flex w-10/12 justify-center  font-serif py-4 m-auto rounded-sm bg-cyan-500 text-black-300 text-sm"
+      >
+        断开链接
+      </button> </>
+}
+
+export default ButtonGroup;
